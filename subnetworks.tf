e02e9838cec306463faf944519ab8789a2d39249
Alexey <root@localhost.localdomain>
2019-10-13 20:15:00 -0400

    reorg files

--- subnetworks.tf
+++ subnetworks.tf
@@ -1,8 +1,3 @@
-resource "google_compute_network" "our_development_network" {
-  name= "devnetwork"
-  auto_create_subnetworks = false
-}
-
 resource "google_compute_subnetwork" "dev-subnet"{
   ip_cidr_range = "10.0.1.0/24"
   name = "devsubnet"
@@ -10,14 +5,6 @@ resource "google_compute_subnetwork" "dev-subnet"{
   region = "us-west1"
 }
 
-resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags = {
-    Name = "terraform-aws-vpc-expample-two"
-  }
-}
 
 resource "aws_subnet" "subnet1" {
   cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
@@ -45,27 +32,3 @@ resource "aws_security_group" "subnetsecurity"{
   }
 }
 
-resource "azurerm_resource_group" "azy_network" {
-  location = "West US"
-  name = "devresgrp"
-}
-
-resource "azurerm_virtual_network" "blue_virtual_network" {
-  address_space = ["10.0.0.0/16"]
-  location = "West US"
-  name = "bluevirtnetwork"
-  dns_servers = ["10.0.0.4", "10.0.0.5"]
-  resource_group_name = "${azurerm_resource_group.azy_network.name}"
-  subnet {
-    name = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-  subnet {
-    address_prefix = "10.0.2.0/24"
-    name = "subnet2"
-  }
-
-  tags = {
-    environment = "blue-world-finder"
-  }
-}
